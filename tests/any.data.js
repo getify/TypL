8acb57b69a189f682ad03a1f227e0e24198f8391
Alen Balja <alen.balja@gmail.com>
2019-02-26 20:08:51 +0100

    #23 Fixed inline code checking

--- tests/any.data.js
+++
@@ -1,4 +0,0 @@
-var a = any``;
-a = "hello";  // OK
-a = 1; // also OK because `a` is still type `any`
-var b = a + 2;   // error: mixed operand types: 'any' and 'number'
--- tests/tests.checker.js
+++ tests/tests.checker.js
@@ -1,7 +1,6 @@
 "use strict";
 var path = require("path");
 var Typl = require(path.join(__dirname, "..", "lib"));
-var fs = require("fs");
 
 // QUnit.test( "Checker: API", function test(assert){
 // 	assert.expect( 1 );
@@ -26,17 +25,12 @@ QUnit.test("Checker: API", function test(assert) {
 
 QUnit.test("Checker: any", function test(assert) {
   assert.expect(2);
-  let contents = fs.readFileSync(path.resolve('tests', 'any.data.js'), "utf-8");
-  let code = `var a = any;
+  let code = `var a = any\`\`;
 	a = "hello";  
 	a = 1; 
 	var b = a + 2;`
   
-  // passing string to checker does not work as expected
-  // let { outputMessages } = Typl.Checker.check(code);
-
-  // passing a content of a file DOES work OK
-  let { outputMessages } = Typl.Checker.check(contents);
+  let { outputMessages } = Typl.Checker.check(code);
 
   let errors = outputMessages.filter(m => m.type == "error")
   assert.equal(outputMessages.length, 4, 'total output length');
